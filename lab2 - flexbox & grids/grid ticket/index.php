2bb9d72157def22d635e2dc4c7ce59d3e67ad660
victor massaer <victor@massaer.be>
2021-02-28 11:50:15 +0100

    added html

---
+++ lab2 - flexbox & grids/grid ticket/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>grid ticket</title>
+</head>
+<body>
+    <main>
+        <div class="head">
+            <h1>WEAREIMD</h1>
+            <p>live programming music</p>
+        </div>
+
+        <img src="qr-code.png" alt="qr code">
+        <h2 class="date">FEB 19</h2>
+        <h2 class="info">admit one</h2>
+        <h2 class="price">ticket €15</h2>
+        <p class="txt">Being frustrated with the css grid is included in the ticket price</p>
+    </main>
+</body>
+</html>
