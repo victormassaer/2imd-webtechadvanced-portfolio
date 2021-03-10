7cd69e2e4cc73ba10880258b3177f105e96c451d
victor massaer <victor@massaer.be>
2021-03-10 15:06:38 +0100

    generated list of names from star wars API

--- lab4 - API/app.js
+++ lab4 - API/app.js
@@ -1,7 +1,27 @@
 class App{
     constructor() {
-        
+        this.getPersons();
+        this.title;
+        this.img;
+        this.person;
     }
+
+    getPersons(){
+        let url = 'https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people/';
+        fetch(url).then(response =>{
+           return response.json();
+        }).then(data =>{
+            this.person = data.results;
+            this.person.forEach(function (arrayItem) {
+                var name = arrayItem.name;
+                document.querySelector('.ad__list').innerHTML = "<li>" + name + "</li>";
+            });
+        })
+        .catch(err =>{
+            console.log(err);
+        });
+    }
+
 }
 
 let app =  new App();
