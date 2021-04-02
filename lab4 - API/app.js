1d19a956cd99d8faa6e1170a09352c1b4ad3c80c
victor massaer <victor@massaer.be>
2021-04-02 17:03:52 +0200

    get the temperature and description from users location via API

--- lab4 - API/app.js
+++ lab4 - API/app.js
@@ -13,8 +13,7 @@ class App{
     gotLocation(result){
         this.lat = result.coords.latitude;
         this.long = result.coords.longitude;
-        console.log(this.lat);
-        console.log(this.long);
+        this.GetWeatherData(this.lat, this.long);
         }
 
     errorLocation(err){
@@ -24,21 +23,27 @@ class App{
     
     
 
-    GetWeatherData(){
-        let url = //connect with weather api;
+    GetWeatherData(lat, long){
+        const weatherKey = 'd94d748308cba7980299e4a2294f6cd9';
+        const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${weatherKey}`;
         fetch(url).then(response =>{
-           return response.json();
+            console.log(response);
+            return response.json();
         }).then(data =>{
-            //get weather data and display temp 
-
+            //get weather data and display temp
+            let temp = data.main.temp;
+            document.querySelector(".ad__temp").innerHTML = data.main.temp + "°C";
+            document.querySelector(".ad__sum").innerHTML = data.weather[0].description;
             //compare temp with plannet on STAR WARS API
-
+            this.getPlanet(this.checkTemp(temp));
             //display picture of plannet 
         })
         .catch(err =>{
             console.log(err);
         });
     }
+
+
 
 }
 
